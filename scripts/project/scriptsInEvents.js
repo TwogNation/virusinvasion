4ea569bf0798bc611b68b11fd79a7913d1c083ac
Matheus N <matheusnaschaves@gmail.com>
2022-08-24 16:15:10 +0200

    virusgame

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,21 @@
+var postMsg = ''
+
+function postText(value){
+	console.log('iFrame sender: ' + value) 
+	parent.postMessage(value, "*")
+}
+
+
+
+const scriptsInEvents = {
+
+		async Es_game_Event69_Act17(runtime, localVars)
+		{
+			postText(runtime.globalVars.finalScore)
+			
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
